ce268160d9e7be0c0a8685a3264a0de784765400
Jan Dědeček <JD@Eugens-MacbookPro.local>
2013-12-02 17:06:44 +0100

    Build fix

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ export PATH="$PATH:$path/node_modules/.bin"
 export KITT_EXT_DIST_PATH="$path/dist"
 export KITT_EXT_BUILD_PATH="$path/build/{NAME}"
 
+shopt -s nullglob;
+
 for f in */manifest.json
 do
   path=$(dirname $f);
