5b69ff364b2f9c4fc4a9ed1b8d7f9baa9dd1494f
Jan Dědeček <JD@Eugens-MacbookPro.local>
2013-12-02 17:23:38 +0100

    Build fix

--- build.sh
+++ build.sh
@@ -7,10 +7,13 @@ export PATH="$PATH:$path/node_modules/.bin"
 export KITT_EXT_DIST_PATH="$path/dist"
 export KITT_EXT_BUILD_PATH="$path/build/{NAME}"
 
-shopt -s nullglob;
+files=$(ls */manifest.json 2> /dev/null | wc -l)
 
-for f in */manifest.json
-do
-  path=$(dirname $f);
-  grunt "--path=$path"
-done
+if [ "$files" != "0" ]
+then
+  for f in */manifest.json
+  do
+    path=$(dirname $f);
+    grunt "--path=$path"
+  done
+fi
