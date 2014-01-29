c210838134c224fc557f84cdca7bab9e3f268e6b
Pavel Zdeněk <pavel@salsitasoft.com>
2014-01-29 17:45:29 +0100

    Ensure function for directory with no extensions

--- build.sh
+++ build.sh
@@ -7,8 +7,11 @@ export PATH="$PATH:$path/node_modules/.bin"
 export KITT_EXT_DIST_PATH="$path/dist"
 export KITT_EXT_BUILD_PATH="$path/build/{NAME}"
 
-for f in */manifest.json
-do
-  path=$(dirname $f);
-  grunt "--path=$path"
-done
+if [ "$files" != "0" ]
+then
+  for f in */manifest.json
+  do
+    path=$(dirname $f);
+    grunt "--path=$path"
+  done
+fi
